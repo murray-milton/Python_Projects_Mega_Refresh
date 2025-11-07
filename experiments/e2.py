9679d577f11917bf08c91ee304c0c5e732640ef7
murray-milton <miltonmln357@gmail.com>
2025-11-07 14:04:30 +0100

    added example for working with dir(), and help()

---
+++ experiments/e2.py
@@ -0,0 +1,4 @@
+"""Working with dir() and help() functions"""
+
+print(dir(list))  # Will return all the methods of the class
+print(help(list))  # Will return by the corresponding help page with class
