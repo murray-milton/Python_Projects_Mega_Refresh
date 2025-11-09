992110df1faad565fabac2b6fbb4fd0df5520f95
murray-milton <miltonmln357@gmail.com>
2025-11-09 10:42:10 +0100

    added an example for a function for even numbers list

--- experiments/e2.py
+++ experiments/e2.py
@@ -1,4 +1,13 @@
-"""Working with dir() and help() functions"""
+# """Working with dir() and help() functions"""
+#
+# print(dir(list))  # Will return all the methods of the class
+# print(help(list))  # Will return by the corresponding help page with class
+def join_numbers(odd_number: [int]) -> list[int]:
+    numbers = []
+    for number in odd_number:
+        if number % 2 == 0:
+            numbers.append(number)
+    return numbers
 
-print(dir(list))  # Will return all the methods of the class
-print(help(list))  # Will return by the corresponding help page with class
+
+print(join_numbers([1, 2, 3, 4, 5]))
