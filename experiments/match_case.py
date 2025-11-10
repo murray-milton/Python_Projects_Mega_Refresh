ef89ac21f1f605ffb6a7a705722fd110eba6aea4
murray-milton <miltonmln357@gmail.com>
2025-11-10 16:01:55 +0100

    added method to handle user input

--- experiments/match_case.py
+++ experiments/match_case.py
@@ -3,14 +3,17 @@
 todos = []
 
 while True:
-    user_action = input("Enter your action (add, show, delete, or quit): ")
+    user_action = (
+        input("Enter your action (add, show, delete, or quit): ").strip().lower()
+    )
 
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-            print(todos)
+            for todo in todos:
+                print(todo)
         case "delete":
             delete_todo = input("Enter the todo you want to delete: e.g: 1, 2, 3")
             del todos[int(delete_todo) - 1]
