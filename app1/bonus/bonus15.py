f30269c72d9af180dc9c62ae2744a31717a58238
murray-milton <miltonmln357@gmail.com>
2026-01-02 11:40:21 +0100

    added an json handling example

--- app1/bonus/bonus15.py
+++ app1/bonus/bonus15.py
@@ -5,14 +5,26 @@ with open("questions.json", "r") as file:
 
 data = json.loads(content)
 
-score = 0
+
 for question in data:
     print(question["question_text"])
     for index, alternative in enumerate(question["alternatives"]):
         print(f"{index + 1}.{alternative}")
     user_choice = int(input("Enter your answer: "))
     question["user_choice"] = user_choice
+
+score = 0
+for index, question in enumerate(data):
     if question["user_choice"] == question["correct_answer"]:
         score += 1
+        result = "Correct Answer"
+    else:
+        result = "Incorrect Answer"
 
-print(data, score)
+    message = (
+        f"{index + 1 } {result} - Your answer: {question['user_choice']}, "
+        f"correct answer: {question['correct_answer']}"
+    )
+    print(message)
+
+print(score, "/", len(data))
